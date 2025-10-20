19629aa3d3d5139b5105ad18be84cbd0a8a10869
AdsonNeres <adsonneres.dev@gmail.com>
2025-10-20 19:46:49 -0300

    testeeeee

--- src/components/ImportStatusModal.tsx
+++ src/components/ImportStatusModal.tsx
@@ -205,7 +205,7 @@ const ImportStatusModal: React.FC<ImportStatusModalProps> = ({ isOpen, onClose, 
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white rounded-lg p-6 max-w-4xl w-full sm:w-auto max-h-[90vh] overflow-y-auto">
         <div className="flex justify-between items-center mb-6 ">
-          <h2 className="text-xl font-semibold">Importar Status de Entregas</h2>
+          <h2 className="text-xl font-semibold">Importar Status de Entrega</h2>
           <button
             onClick={onClose}
             className="text-gray-500 hover:text-gray-700"
